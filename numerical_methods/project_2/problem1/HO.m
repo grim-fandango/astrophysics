8cfc355554f0ff32b68eda6b1e169b8a0a9e1496
Jason Robertson <jkrobertson@live.com>
2022-03-08 00:17:10 +0000

    add x-v plane plot

--- numerical_methods/project_2/problem1/HO.m
+++ numerical_methods/project_2/problem1/HO.m
@@ -12,20 +12,37 @@ Xa = X;         % initial position for analytical solution
 for j=1:N+1
      time(j)= t;
      E(:,j) = Xe;
+     Ve(:,j) = Xe(1)
      R(:,j) = Xr;
+     Vr(:,j) = Xr(1)
      A(:,j) = Xa;
+     Va(:,j) = Xa(1)
      Xe = Eul(h,t,Xe);
      Xr = RK4(h,t,Xr);
      Xa = Ana(h,t,Xa);
+     
      t  = t+h;
 end
+
+figure(1)
 plot(time,E(1,:))
 hold on
 plot(time,R(1,:),'r')
 hold on
 plot(time,A(1,:),'g')
 
-
 xlabel('t','FontSize',20)
 ylabel('x','FontSize',20)
 set(gca, 'Fontsize', 15)
+
+figure(2)
+plot(E(1,:),Ve(1,:))
+hold on
+plot(R(1,:),Vr(1,:))
+hold on
+plot(A(1,:),Va(1,:))
+
+xlabel('x','FontSize',20)
+ylabel('v','FontSize',20)
+set(gca, 'Fontsize', 15)
+

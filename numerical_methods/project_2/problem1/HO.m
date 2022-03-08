e11588a9576ae234d0794ce9ed7c3091ade08fd7
Jason Robertson <jkrobertson@live.com>
2022-03-08 00:03:42 +0000

    Analytical line added

--- numerical_methods/project_2/problem1/HO.m
+++ numerical_methods/project_2/problem1/HO.m
@@ -7,17 +7,23 @@ X(2)= 0;      % initial velocity
 N = tf/h;       % number of time-steps
 Xr = X;         % initial position for RK4
 Xe = X;         % initial position for Euler
+Xa = X;         % initial position for analytical solution
+
 for j=1:N+1
      time(j)= t;
      E(:,j) = Xe;
      R(:,j) = Xr;
+     A(:,j) = Xa;
      Xe = Eul(h,t,Xe);
      Xr = RK4(h,t,Xr);
+     Xa = Ana(h,t,Xa);
      t  = t+h;
 end
 plot(time,E(1,:))
 hold on
 plot(time,R(1,:),'r')
+hold on
+plot(time,A(1,:),'g')
 
 
 xlabel('t','FontSize',20)
